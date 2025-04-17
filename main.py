34d2b268ea9896ae5dcd0b1d2c39952775aac90c
vmaster7i <reflectionmechanism8@gmail.com>
2025-04-17 16:29:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import openai
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
 
-openai.api_key = "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"
-TELEGRAM_TOKEN = "8181071639:AAHHUfI9dwUvVuve7wqd-E2SEJ6GBMlNly8"
+openai.api_key ="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"
+TELEGRAM_TOKEN ="8181071639:AAHHUfI9dwUvVuve7wqd-E2SEJ6GBMlNly8"
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
